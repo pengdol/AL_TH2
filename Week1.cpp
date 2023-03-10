58c0cdc87d8b78bde30cbd6678aec139c10025b6
pengdololololololololol <108115799+pengdol@users.noreply.github.com>
2023-03-10 16:38:14 +0900

    Create Week1.cpp

---
+++ Week1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cstdio>
+#include <string>
+#include <stack>
+#include <queue>
+#include <algorithm>
+using namespace std;
+/*
+1. ranking system
+
+int main(){
+  pair<int, pair<int, int>> p[100001];
+  int n;
+  scanf("%d", &n);
+  for(int i = 0; i < n; i++)
+    scanf("%d %d %d", &p[i].first, &p[i].second.first, &p[i].second.second);
+  sort(p, p + n, greater<>());
+  for(int i = 0; i < n; i++)
+    printf("%d %d %d\n", p[i].first, p[i].second.first, p[i].second.second);
+}
+
+2.
+*/
+int main(){
+  
+}
