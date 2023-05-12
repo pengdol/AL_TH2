cfbc029e822db0cfa6d827286139b511dcfad04d
pengdololololololololol <108115799+pengdol@users.noreply.github.com>
2023-05-12 16:24:51 +0900

    Create Week9.cpp

---
+++ Week9.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+/*
+1. 개미
+int main() {
+  int w, h, p, q, t, x, y;
+  cin >> w >> h >> p >> q >> t;
+  x = (p + t) % (w * 2);
+  y = (q + t) % (h * 2);
+  if(x > w)
+    x = w * 2 - x;
+  if(y > h)
+    y = h * 2 - y;
+}
+
+2. 소수 구하기
+
+int main(){
+  int a[1000001] = { 0, };
+  int m, n;
+  cin >> m >> n;
+  for(int i = 2; i <= n; i++)
+    a[i] = 1;
+  for(int i = 4; i <= n; i+=2)
+    a[i] = 0;
+  for(int i = 6; i <= n; i+=3)
+    a[i] = 0;
+  for(int i = m; i <= n; i++) 
+    if(a[i] == 1) cout << i << '\n';
+}
+
+3. 가로수
+*/
